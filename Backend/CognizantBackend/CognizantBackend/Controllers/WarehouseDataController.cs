a7ba869b52b3c3f0d885a016dd98a64ecee2efaf
mohamedhamed1234 <mohamed.hamed123@gmail.com>
2021-09-02 16:22:18 +0200

    Add Unit Tests

--- Backend/CognizantBackend/CognizantBackend/Controllers/WarehouseDataController.cs
+++ Backend/CognizantBackend/CognizantBackend/Controllers/WarehouseDataController.cs
@@ -8,21 +8,29 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
 using CognizantBackend.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace CognizantBackend.Controllers
 {
     [Route("api/[controller]")]
     public class WarehouseDataController : Controller
     {
-        
+        private readonly IConfiguration _configuration;
+ 
+        public WarehouseDataController(IConfiguration configuration)
+        {
+
+            _configuration = configuration;
+        }
 
         [HttpGet("[action]")]
         public IEnumerable<Warehouse> WarehouseCars()
         {
 
-          
 
-            DataLoader dataLoader = new DataLoader("warehouses.json");
+        var filePathath = _configuration["FilePath"];
+
+            DataLoader dataLoader = new DataLoader(filePathath);
             Warehouses warehouses = dataLoader.LoadData();
 
 
---
+++ Backend/CognizantBackend/CongnizantUnitTestProject/WarehouseCarsUnitTest.cs
@@ -0,0 +1,54 @@
+﻿using CognizantBackend.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace CongnizantUnitTestProject
+{
+    public class WarehouseCarsUnitTest
+    {
+
+        private readonly IConfiguration _configuration;
+        public WarehouseCarsUnitTest()
+        {
+
+          
+        }
+        public int getPath()
+        {
+            return 0;
+        }
+
+        [Fact]
+        public void IndexGet_ReturnsWarehouseCars()
+        {
+            // Arrange
+
+            var inMemorySettings = new Dictionary<string, string> {
+                {"FilePath", "../../../warehouses.json"},
+                };
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+
+            var controller = new WarehouseDataController(configuration);
+
+           
+            // Act
+
+            System.Collections.Generic.IEnumerable<CognizantBackend.Core.Warehouse> result = controller.WarehouseCars().ToList();
+
+            // Assert
+            Assert.Equal(4, result.Count());
+
+        }
+    }
+}
